ce7d44d32f349a29c2d4cb1ae57ade23f862c0b0
Victor Manuel Largo Vasquez <159088039+Victorl541@users.noreply.github.com>
2025-02-23 11:49:22 -0500

    Hechos de las conexiones direccionales de cada grafo y el costo de sus aristas

--- Actividades_Prolog/Act02/03-Backtraking y SLD.pl
+++ Actividades_Prolog/Act02/03-Backtraking y SLD.pl
@@ -1,1 +1,15 @@
+%---hechos de las conexiones direccionales de cada grafo y el costo de sus aristas---
 
+conectadoA(vancouver, edmonton, 16).
+conectadoA(vancouver, calgary, 13).
+
+conectadoA(edmonton, saskatoon, 12).
+
+conectadoA(calgary, edmonton, 4).
+conectadoA(calgary, regina, 14).
+
+conectadoA(regina, saskatoon, 7).
+conectadoA(regina, winnipeg, 4).
+
+conectadoA(saskatoon, calgary, 9).
+conectadoA(saskatoon, winnipeg, 4).
