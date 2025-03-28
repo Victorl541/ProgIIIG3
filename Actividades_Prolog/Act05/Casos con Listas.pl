d17327a8ef21fbb3ed55282868a5399cb5c197ba
Victor Manuel Largo Vasquez <159088039+Victorl541@users.noreply.github.com>
2025-03-28 13:36:04 -0500

    Creación de los hechos usando listas

--- Actividades_Prolog/Act05/Casos con Listas.pl
+++ Actividades_Prolog/Act05/Casos con Listas.pl
@@ -1,1 +1,16 @@
+% --- Hechos familia Simpson usando listas --- 
+
+esHombre([bart, homero, abraham, herbert, clancy]).
+
+esMujer([marge, lisa, selma, patty, maggie, ling, mona, jacqueline]).
+
+
+esPadre([padre(abraham, homero), padre(abraham, herbert), padre(clancy, selma), padre(clancy, patty), padre(clancy, marge),
+    padre(homero, bart), padre(homero, lisa), padre(homero, maggie)
+]).
+
+
+esMadre([madre(marge, lisa), madre(marge, bart), madre(marge, maggie), madre(selma, ling), madre(jacqueline, patty), madre(jacqueline, selma), madre(jacqueline, marge),
+    madre(mona, homero), madre(mona, herbert)
+]).
 
