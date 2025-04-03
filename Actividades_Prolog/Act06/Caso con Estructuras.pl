1da5fa38ff5a6d5a41756b7f3368ceb5bcde32cf
Victor Manuel Largo Vasquez <159088039+Victorl541@users.noreply.github.com>
2025-04-02 19:53:31 -0500

    Se define una lista con persona(Nombre, Género, Padre, Madre)

--- Actividades_Prolog/Act06/Caso con Estructuras.pl
+++ Actividades_Prolog/Act06/Caso con Estructuras.pl
@@ -1,1 +1,17 @@
 
+
+personas([
+    persona(bart, hombre, homero, marge),
+    persona(homero, hombre, abrajam, mona),
+    persona(abrajam, hombre, _, _),
+    persona(herbert, hombre, abrajam, mona),
+    persona(clancy, hombre, _, _),
+    persona(marge, mujer, clancy, jacqueline),
+    persona(lisa, mujer, homero, marge),
+    persona(selma, mujer, clancy, jacqueline),
+    persona(patty, mujer, clancy, jacqueline),
+    persona(maggie, mujer, homero, marge),
+    persona(ling, mujer, _, selma),
+    persona(mona, mujer, _, _),
+    persona(jacqueline, mujer, _, _) ]).
+
