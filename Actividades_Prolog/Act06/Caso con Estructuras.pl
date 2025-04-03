b62f35aef620ffc45fc9f690103b3770cce7e4be
Victor Manuel Largo Vasquez <159088039+Victorl541@users.noreply.github.com>
2025-04-02 21:05:22 -0500

    implementación de reglas que definen las relaciones en los simpson

--- Actividades_Prolog/Act06/Caso con Estructuras.pl
+++ Actividades_Prolog/Act06/Caso con Estructuras.pl
@@ -36,3 +36,44 @@ relaciones([
     madre(mona, homero),
     madre(mona, herbert)
 ]).
+
+
+% --- Reglas ---
+
+esHombre(X) :- personas(Lista), member(persona(X, hombre), Lista).
+esMujer(X) :- personas(Lista), member(persona(X, mujer), Lista).
+
+esPadreDe(P, Hijo) :- relaciones(Lista), member(padre(P, Hijo), Lista).
+esMadreDe(M, Hijo) :- relaciones(Lista), member(madre(M, Hijo), Lista).
+
+abueloPaternoDe(X, Y) :- esHombre(X), esPadreDe(X, Z), esPadreDe(Z, Y).
+abuelaPaternaDe(X, Y) :- esMujer(X), esMadreDe(X, Z), esPadreDe(Z, Y).
+
+abueloMaternoDe(X, Y) :- esHombre(X), esPadreDe(X, Z), esMadreDe(Z, Y).
+abuelaMaternaDe(X, Y) :- esMujer(X), esMadreDe(X, Z), esMadreDe(Z, Y).
+
+esHermanoDe(X, Y) :- 
+    esHombre(X), esPadreDe(P, X), esPadreDe(P, Y), esMadreDe(M, X), esMadreDe(M, Y), X \= Y.
+
+esHermanaDe(X, Y) :- 
+    esMujer(X), esPadreDe(P, X), esPadreDe(P, Y), esMadreDe(M, X), esMadreDe(M, Y), X \= Y.
+
+esTioDe(X, Y) :- esHermanoDe(X, P), esPadreDe(P, Y).
+esTioDe(X, Y) :- esHermanoDe(X, M), esMadreDe(M, Y).
+
+esTiaDe(X, Y) :- esHermanaDe(X, P), esPadreDe(P, Y).
+esTiaDe(X, Y) :- esHermanaDe(X, M), esMadreDe(M, Y).
+
+sonPrimos(X, Y) :- 
+    esPadreDe(P1, X), esPadreDe(P2, Y), esHermanoDe(P1, P2), X \= Y.
+sonPrimos(X, Y) :- 
+    esMadreDe(M1, X), esMadreDe(M2, Y), esHermanaDe(M1, M2), X \= Y.
+sonPrimos(X, Y) :- 
+    esPadreDe(P, X), esMadreDe(M, Y), esHermanoDe(P, M), X \= Y.
+sonPrimos(X, Y) :- 
+    esMadreDe(M, X), esPadreDe(P, Y), esHermanaDe(M, P), X \= Y.
+
+esPrimoDe(X, Y) :- esHombre(X), sonPrimos(X, Y).
+esPrimaDe(X, Y) :- esMujer(X), sonPrimos(X, Y).
+
+
