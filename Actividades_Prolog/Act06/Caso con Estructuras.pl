df5da2550fffa1939e5fa42cd2018f38f5435c06
Victor Manuel Largo Vasquez <159088039+Victorl541@users.noreply.github.com>
2025-04-02 20:46:35 -0500

    Creacion de las estructuras de personas y relaciones

--- Actividades_Prolog/Act06/Caso con Estructuras.pl
+++ Actividades_Prolog/Act06/Caso con Estructuras.pl
@@ -1,1 +1,38 @@
+% --- Definición de relaciones de los Simpson usando estructuras y listas ---
 
+personas([
+    persona(bart, hombre),
+    persona(homero, hombre),
+    persona(abrajam, hombre),
+    persona(herbert, hombre),
+    persona(clancy, hombre),
+    persona(marge, mujer),
+    persona(lisa, mujer),
+    persona(selma, mujer),
+    persona(patty, mujer),
+    persona(maggie, mujer),
+    persona(ling, mujer),
+    persona(mona, mujer),
+    persona(jacqueline, mujer)
+]).
+
+
+relaciones([
+    padre(abrajam, homero),
+    padre(abrajam, herbert),
+    padre(clancy, selma),
+    padre(clancy, patty),
+    padre(clancy, marge),
+    padre(homero, bart),
+    padre(homero, lisa),
+    padre(homero, maggie),
+    madre(marge, lisa),
+    madre(marge, bart),
+    madre(marge, maggie),
+    madre(selma, ling),
+    madre(jacqueline, patty),
+    madre(jacqueline, selma),
+    madre(jacqueline, marge),
+    madre(mona, homero),
+    madre(mona, herbert)
+]).
